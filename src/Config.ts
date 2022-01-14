1a35cb4d64eda8a9a394c3effa2a4339ba5318e4
Hanul <hanul@hanul.me>
2022-01-14 23:23:44 +0900

    bug fix

--- src/Config.ts
+++ src/Config.ts
@@ -28,6 +28,6 @@ export default {
         VirtualBitcoin: "0x84e7ae4897b3847b67f212aff78bfbc5f700aa40",
         VirtualNewLibertyStandard: "0x9C71228aAE840d19d3a4447071A7D6708C9046A9",
         VNLSBuyOrderBook: "0x14e90440d54014B8A85ff9F25734B1f2c51d16cc",
-        VNLSSellOrderBook: "0x36fBC9482449BeFE6F8c5d7d802Cae806c6826bf",
+        VNLSSellOrderBook: "0x079e1750AD6f7Aa6618CDeCBC39186FB7DbED925",
     },
 };
